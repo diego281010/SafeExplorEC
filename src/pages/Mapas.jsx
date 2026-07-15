fb2bee1406cb12e4f8217eeb2f2e316b2b926c5d
diego281010 <diego.almeida03@epn.edu.ec>
2026-07-14 21:21:25 -0500

    About Acceso denegado y Mapas

---
+++ src/pages/Mapas.jsx
@@ -0,0 +1,11 @@
+import MapSection from '../components/map/Map';
+
+function Mapas() {
+  return (
+    <div className="mapas-page">
+      <MapSection />
+    </div>
+  );
+}
+
+export default Mapas;
