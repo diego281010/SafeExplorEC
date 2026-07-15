b093781bbcb4bf5e17a577eab849be3196199a67
alejandro4418 <alejandro.aguirre@epn.edu.ec>
2026-07-14 21:16:50 -0500

    Paginas Perfil, Quejas y Zonas creadas, y AuthContext.jsx creado

---
+++ src/pages/Quejas.jsx
@@ -0,0 +1,11 @@
+import Complaints from "../components/complaints/Complaints";
+
+function Quejas() {
+  return (
+    <div className="quejas-page">
+      <Complaints />
+    </div>
+  );
+}
+
+export default Quejas;
---
+++ src/pages/Zonas.jsx
@@ -0,0 +1,11 @@
+import ZonasList from "../components/zones/ZonasList";
+
+function Zonas() {
+  return (
+    <div className="zonas-page">
+      <ZonasList />
+    </div>
+  );
+}
+
+export default Zonas;
